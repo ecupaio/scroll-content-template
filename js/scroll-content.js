11e0112836d911e6bbe8d7b778c3b8573e9ae926
Ed Cupaioli <cupaiolie@dnc.org>
2016-10-18 13:24:28 -0400

    added scroll functions for navigator and section name display

--- js/scroll-content.js
+++ js/scroll-content.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
-    /*$(window).bind('mousewheel DOMMouseScroll', function(event) {
-        if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-            //scrollPrev();
-        } else {
-           //scrollNext();
-        }
-    });*/
-    $(document).keydown(function(e) {
+    $(window).keydown(function(e) {
         if (e.keyCode == 38) {
             scrollPrev();
         }
@@ -14,15 +7,57 @@ $(document).ready(function() {
             scrollNext();
         }
     });
-
     function scrollNext() {
         $('body,html').animate({
             scrollTop: $(".content-section").next(".content-section").offset().top
         }, '1000', 'swing');
+
     }
     function scrollPrev() {
         $('body,html').animate({
             scrollTop: $(".content-section").prev(".content-section").offset().top
         }, '1000', 'swing');
     }
+    var sectionData;
+    function scrollDirect() {
+    	$('body,html').animate({
+            scrollTop: $('.content-section[data-section="'+sectionData+'"]').offset().top
+        }, '1000', 'swing');
+    }
+    
+    var sectionIndex; 
+    $('.content-section').each(function(index, obj){
+    	sectionIndex = index + 1;
+    	$('.quick-jump').append('<div class="quick-jump-item" data-section="'+sectionIndex+'" ><i class="fa fa-circle"></i></div>');
+    });
+    
+    $('.quick-jump-item').click(function () {
+    	sectionData = $(this).data('section');
+    	scrollDirect();
+    });
+    $(window).scroll( function() {
+    	 var scrollTop = $(window).scrollTop();
+    	 $('.content-section').each(function() {
+    	 	var contentTop = $(this).offset().top
+    	 	sectionData = $(this).data('section');
+    	 	var contentHeader = $(this).find('.section-header').text();
+		    if (contentTop - scrollTop < 100 ) {
+		    	$('.current-section').text(contentHeader);
+		    	$('.site-title').hide();
+		    	$('.current-section').show();
+		    	//highlight quick jump item
+		    	$('.quick-jump-item').removeClass('active');
+		    	$('.quick-jump-item[data-section="'+sectionData+'"]').addClass('active');	
+		    }
+		});
+    });
+    
+    $('.scroll-btn').click(function () {
+    	if ($(this).hasClass('up')) {
+    		scrollPrev();
+    	} else if ($(this).hasClass('down')) {
+    		scrollNext();
+    	}
+    });
+
 });
