4c0e759bd5c35f608ea00312c61dcdf13ca4272c
Ed Cupaioli <cupaiolie@dnc.org>
2016-11-21 13:37:06 -0500

    removed scroll animation

--- js/scroll-content.js
+++ js/scroll-content.js
@@ -7,17 +7,7 @@ $(document).ready(function() {
             scrollNext();
         }
     });
-    $('body,html').on('mousewheel', function(event, delta) {
-        if ($('body,html').is(':animated')) {
-            return false;
-        }
-        if (delta < 0) {
-            scrollNext();
-        }
-        if (delta > 0) {
-            scrollPrev();
-        }
-    });
+    
 
     function scrollNext() {
         $('body,html').animate({
